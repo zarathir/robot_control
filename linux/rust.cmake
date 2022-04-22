36ac2588f9367d87ccf693d4cceee7ff08291d20
Michael Tal <michael.tal@protonmail.ch>
2022-04-22 22:29:16 +0200

    changed how corrosion is searched

--- linux/rust.cmake
+++ linux/rust.cmake
@@ -2,15 +2,15 @@
 # many dependencies we would need to install Corrosion on the system.
 # See instructions on https://github.com/AndrewGaspar/corrosion#cmake-install
 # Once done, uncomment this line:
-find_package(Corrosion REQUIRED)
+#find_package(Corrosion REQUIRED)
 
-#include(FetchContent)
+include(FetchContent)
 
-#FetchContent_Declare(
-#    Corrosion
-#    GIT_REPOSITORY https://github.com/AndrewGaspar/corrosion.git
-#    GIT_TAG origin/master # Optionally specify a version tag or branch here
-#)
+FetchContent_Declare(
+    Corrosion
+    GIT_REPOSITORY https://github.com/AndrewGaspar/corrosion.git
+    GIT_TAG origin/master # Optionally specify a version tag or branch here
+)
 
 corrosion_import_crate(MANIFEST_PATH ../zenoh_client/Cargo.toml)
 
